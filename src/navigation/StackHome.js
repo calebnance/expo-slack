daef7e68075bbf20da8562ef9f6fa22f93a80d5e
Caleb Nance <calebnance@gmail.com>
2019-07-06 01:29:48 -0400

    stacks setup

---
+++ src/navigation/StackHome.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { createStackNavigator } from 'react-navigation';
+
+import Home from '../screens/Home';
+import Album from '../screens/Album';
+import SvgTabHome from '../components/icons/Svg.TabHome';
+
+const Icon = ({ focused }) => <SvgTabHome active={focused} />;
+
+Icon.propTypes = {
+  // required
+  focused: PropTypes.bool.isRequired
+};
+
+export default createStackNavigator(
+  {
+    Home,
+    Album
+  },
+  {
+    headerMode: 'none',
+    initialRouteName: 'Home',
+    navigationOptions: {
+      tabBarLabel: 'Home',
+      tabBarIcon: Icon
+    }
+  }
+);
